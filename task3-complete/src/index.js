5842d83e7a124796bba2f1666e7ccb5decbf5ac8
Ash Whiting <ash_whiting@hotmail.com>
2019-03-05 16:07:33 +0000

    removed extra stylesheet import

--- task3-complete/src/index.js
+++ task3-complete/src/index.js
@@ -4,7 +4,6 @@ import App from "./components/App";
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/Index.css';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
